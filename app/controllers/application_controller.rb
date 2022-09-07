86ed75b0c44d54159568a0f95928193f36c5199a
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-09-07 22:15:28 +0300

    Post action has been added under controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,20 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/create_posts" do
-    create_post = Post.all
-    create_post.to_json
+  get '/create_posts' do
+    post_content = Post.all
+    post_content.to_json
+  end
+
+# Get post based on individual id
+  # get '/create_posts/:id' do
+  #   post_finder = Post.find(params[:id])
+  #   post_finder.to_json
+  # end
+
+  post '/create_comments/:id' do
+    comment = Comment.creare(reply: params[:reply])
+    comment.to_json
   end
 
 end
